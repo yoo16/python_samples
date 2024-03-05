1af393909ba8920e1798ba3c18b20b3101500fa4
yoo16 <yohei.yoshikawa@gmail.com>
2024-03-05 17:45:10 +0900

    update

--- array_list.py
+++ array_list.py
@@ -1,5 +1,13 @@
 import random
 
+drinks = ()
+drinks = ("") * 3
+drinks = ("コーヒー", "紅茶", "ほうじ茶")
+print(drinks)
+
+selectDrink = drinks[1]
+print(selectDrink)
+
 strings = [""] * 3
 
 drinks = ["コーヒー", "紅茶", "ほうじ茶"]
@@ -19,7 +27,15 @@ numbers[2] = 72
 
 print(numbers)
 
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+numbers = numbers1 + numbers2
+print(numbers)
+
+numbers *= 3
+print(numbers)
+
 # ランダムな整数を生成
 index = random.randint(0, 2)
-hands = ["グー", "チョキ", "パー"]
+hands = ("グー", "チョキ", "パー")
 print(hands[index])
--- repeat.py
+++ repeat.py
@@ -1,3 +1,47 @@
-numbers = range(1, 10)
+import random
+
+numbers = [1, 2, 3, 4, 5]
+total = 0
 for number in numbers:
-    print(number)
+    total += number
+print(total)
+
+for x in range(3):
+    for y in range(3):
+        plot = str(x) + "-" + str(y)
+        print(plot)
+
+drinks = ["コーヒー", "紅茶", "ほうじ茶"]
+for index, drink in enumerate(drinks):
+    message = str(index) + ":" + drink
+    print(message)
+
+count = 0
+while count <= 10:
+    count += 1
+print(count)
+
+count = 0
+while True:
+    count += 1
+    if (count == 5):
+        print("Finish!")
+        break
+print(count)
+
+items = ["PC", "TV", "エアコン"]
+for item in items:
+    print(item)
+
+for x in range(1, 10):
+    for y in range(1, 10):
+        print(x * y, end=" ")
+    print("")
+
+marks = ["⚫︎", "⚪︎︎"]
+for x in range(0, 5):
+    print("|", end="")
+    for y in range(0, 5):
+        index = random.randint(0, 1)
+        print(marks[index], end="|")
+    print("")
